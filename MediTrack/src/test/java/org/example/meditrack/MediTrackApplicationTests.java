9936803cd5aa55be782b8d480c0034f6e9432c3d
Rajitha Yedla <rajitha.yedla82@gmail.com>
2026-02-09 23:41:16 +0530

    [MediTrack] : InitialCommit

---
+++ MediTrack/src/test/java/org/example/meditrack/MediTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
