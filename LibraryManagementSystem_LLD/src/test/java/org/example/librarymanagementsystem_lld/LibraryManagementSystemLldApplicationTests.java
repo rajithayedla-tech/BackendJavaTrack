613b6f84cabb6203dcd28380de5e28f78a72489a
Rajitha Yedla <rajitha.yedla82@gmail.com>
2026-02-21 16:10:18 +0530

    <LearnerManagement> Empty project commit

---
+++ LibraryManagementSystem_LLD/src/test/java/org/example/librarymanagementsystem_lld/LibraryManagementSystemLldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.librarymanagementsystem_lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemLldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
