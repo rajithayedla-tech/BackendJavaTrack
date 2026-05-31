c4eea037897835cdcd11214286a033383e74f807
Rajitha Yedla <rajitha.yedla82@gmail.com>
2026-05-31 13:43:08 +0530

    [PayflowUPI] new project commit

---
+++ PayflowUPI/src/main/java/org/example/payflowupi/PayflowUpiApplication.java
@@ -0,0 +1,13 @@
+package org.example.payflowupi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowUpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayflowUpiApplication.class, args);
+    }
+
+}
---
+++ PayflowUPI/src/test/java/org/example/payflowupi/PayflowUpiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.payflowupi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayflowUpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
